dcbe5184725399e456591be561eeab4b711120d3
Bryan A <shodivine@gmail.com>
2012-09-26 20:20:42 -0500

    * modules/scanner/service_scan.py   -- Added dhcp scanning for rogue DHCP discovery

--- modules/scanner/service_scan.py
+++ modules/scanner/service_scan.py
@@ -12,6 +12,7 @@ services = {
 			'telnet':23,
 			'smtp' : 24,
 			'dns' : 53,
+			'dhcp' : 67,
 			'http' : 80,
 			'snmp' : 161
 		    }
@@ -64,11 +65,17 @@ def service_scan ( block, service ):
 			ip = r[ARP].getfieldval('psrc')
 			if type(service) is list:
 				for port in service:
+					if port == 67:
+						dhcp_scan()
+						continue
 					pkt = sr1(IP(dst=ip)/TCP(flags='S',dport=port),timeout=1)
 					if not pkt is None and pkt[TCP].getfieldval('flags') == 18L:
 						print '\t[+] %s'%(ip)
 						print '\t  %d \t %s'%(pkt[TCP].sport, 'open')
 			else:
+				if service == 67:
+					dhcp_scan()
+					return 
 				pkt = sr1(IP(dst=ip)/TCP(flags='S',dport=service),timeout=1)
 				if not pkt is None and pkt[TCP].getfieldval('flags') == 18L:
 					print '\t[+] %s'%(ip)
@@ -77,3 +84,22 @@ def service_scan ( block, service ):
 	except Exception, j:
 		print '[dbg] error: ', j
 	print '\n'
+
+#
+# Scan for DHCP servers
+#
+def dhcp_scan():
+	conf.checkIPaddr = False
+	fm, hw_addr = get_if_raw_hwaddr(conf.iface)
+	dhcp_discovery = Ether(dst="ff:ff:ff:ff:ff:ff")/IP(src="0.0.0.0",dst="255.255.255.255")/UDP(sport=68,dport=67)/BOOTP(chaddr=hw_addr)/DHCP(options=[("message-type","discover"),"end"])
+	answered,unanswered = srp(dhcp_discovery, multi=True, timeout=10)
+	
+	# check/parse responses
+	if len(answered) > 0:
+		print '[+] Responding DHCP servers'
+		print '\t   {0:21} {1:25}'.format('[IP]', '[MAC]')
+		for i, f in answered:
+			print '\t{0:20} {1:20}'.format(f[IP].src, f[Ether].src)
+		print '\n'
+	else:
+		print '[-] No DHCP servers found.'
